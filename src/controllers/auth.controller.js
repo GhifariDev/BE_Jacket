005ab0851857a5aa6e8de641993a13cebee9c99c
Ghifari <fauzighifari5@gmail.com>
2025-08-04 20:49:46 +0700

    last

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -52,12 +52,12 @@ const login = async (req, res) => {
     );
 
     // ✅ Simpan token ke HTTP-only cookie
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production', // hanya HTTPS di production
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 hari
-    });
+  res.cookie('token', token, {
+  httpOnly: true,
+  secure: true,
+  sameSite: 'None', // <— penting untuk cross-origin seperti vercel dan replit
+});
+
 
     res.json({
       message: 'Login berhasil',
